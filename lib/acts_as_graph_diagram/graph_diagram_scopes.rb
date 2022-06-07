960d75777ea5fe0fc97b95fb5767b32f7190ad7e
smapira <smapira@routeflags.com>
2022-06-07 22:49:25 +0900

    :memo: Enhance rdoc

--- lib/acts_as_graph_diagram/graph_diagram_scopes.rb
+++ lib/acts_as_graph_diagram/graph_diagram_scopes.rb
@@ -6,12 +6,15 @@ module ActsAsGraphDiagram # :nodoc:
 
     included do
       # returns Edge records where destination is the record passed in.
+      # @param [Node] node
       scope :select_destinations, ->(node) { where(destination_id: node.id, destination_type: node.class.name) }
 
       # returns Edge records where departure is the record passed in.
+      # @param [Node] node
       scope :select_departures, ->(node) { where(departure_id: node.id, departure_type: node.class.name) }
 
       # returns Edge records where departure or destination are the record passed in.
+      # @param [Node] node
       scope :select_connections, ->(node) { select_destinations(node).or(select_departures(node)) }
     end
   end
--- lib/acts_as_graph_diagram/node.rb
+++ lib/acts_as_graph_diagram/node.rb
@@ -16,56 +16,77 @@ module ActsAsGraphDiagram # :nodoc:
 
     module InstanceMethods # :nodoc:
       # Creates a new destination record for this instance to connect the passed object.
+      # @param [Node] node
+      # @return [Edge]
       def add_destination(node)
         departures.select_destinations(node).first_or_create!
       end
 
       # Creates a new departure record for this instance to connect the passed object.
+      # @param [Node] node
+      # @return [Edge]
       def add_departure(node)
         destinations.select_departures(node).first_or_create!
       end
 
       # Creates a new undirected connection record for this instance to connect the passed object.
+      # @param [Node] node
+      # @return [Edge]
       def add_connection(node)
         Edge.where(destination: node, directed: false, departure: self).first_or_create!
       end
 
       # Returns a destination node record for the current instance.
+      # @param [Node] node
+      # @return [Edge]
       def get_destination(node)
         departures.select_destinations(node).first
       end
 
       # Deletes the destination record if it exists.
+      # @param [Node] node
+      # @return [Edge|nil]
       def remove_destination(node)
         get_destination(node).try(:destroy)
       end
 
       # Returns a departure node record for the current instance.
+      # @param [Node] node
+      # @return [Edge]
       def get_departure(node)
         destinations.select_departures(node).first
       end
 
       # Deletes the destination record if it exists.
+      # @param [Node] node
+      # @return [Edge|nil]
       def remove_departure(node)
         get_departure(node).try(:destroy)
       end
 
       # Returns a undirected node record for the current instance.
+      # @param [Node] node
+      # @return [Edge]
       def get_connection(node)
         Edge.select_connections(node).first
       end
 
       # Deletes the undirected record if it exists.
+      # @param [Node] node
+      # @return [Edge|nil]
       def remove_connection(node)
         get_connection(node).try(:destroy)
       end
 
       # Returns true if this instance is connecting the object passed as an argument.
+      # @param [Node] node
+      # @return [Boolean]
       def connecting?(node)
         node.connecting_count.positive?
       end
 
       # Returns the number of objects this instance is following.
+      # @return [Integer]
       def connecting_count
         Edge.select_destinations(self).count + Edge.select_departures(self).count
       end
